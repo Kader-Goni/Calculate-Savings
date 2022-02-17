8fc99d6a66a1dfb37a0b3e108bf3549ac6fea291
Kader-Goni <kadergoni35@gmail.com>
2022-02-17 23:52:50 +0600

    All Done

--- js/app.js
+++ js/app.js
@@ -1,44 +1,79 @@
-// Calculate Function 
+
+// =====Calculate Function =========
 function calculate(){
-    const foodCost = document.getElementById('food-input').value;
-    const rentCost = document.getElementById('rent-input').value;
-    const clothesCost = document.getElementById('cloth-input').value;
-    let totalCost = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothesCost);
+    const foodCost = document.getElementById('food-input');
+    const foodCostVlue = parseFloat(foodCost.value);
+
+    const rentCost = document.getElementById('rent-input');
+    const rentCostVlue = parseFloat(rentCost.value);
+
+    const clothesCost = document.getElementById('cloth-input');
+    const clothesCostVlue = parseFloat(clothesCost.value);
+
+    let totalCost = parseInt(foodCostVlue + rentCostVlue + clothesCostVlue);
 
     // expenses calculate
     const expenses = document.getElementById('total-expenses');
-    const expensesText = expenses.innerText;
-    const totalExpenses = expensesText;
-    expenses.innerText = parseFloat(totalExpenses) + totalCost;
-    expenses.innerText = totalCost;
+
+    // error handaler
+    if(foodCostVlue > 0 && rentCostVlue > 0 && clothesCostVlue > 0){
+        expenses.innerText = totalCost;
+    }else{
+        validation("alert-1");
+        return expenses.innerText = '00';
+    }
 
     // balance calculate
     const incomeValue = document.getElementById('income-input').value;
     const balanceOutput = document.getElementById('balance');
-    let totalBalance = parseFloat(incomeValue) - totalCost;
-    balanceOutput.innerText = totalBalance;
 
-    return totalBalance;
+    // error handaler
+    if(incomeValue < totalCost){
+        validation("alert-2");
+        return balanceOutput.innerText = '00'
+    }
+    else if(incomeValue > 0){
+        balanceOutput.innerText = parseFloat(incomeValue) - totalCost;
+    }else{
+        validation("alert-1");
+        return balanceOutput.innerText = '00';
+    }
 }
 
-
-// SaveMoney Function 
+// =====SaveMoney Function =========
 function saveMoney(){
     // saving balance
     const incomeValue = document.getElementById('income-input').value;
     const saveInput = document.getElementById('saving-input').value;
     const saveMoney = parseFloat(incomeValue) * parseFloat(saveInput) / 100 ;
 
+    
+
     // saving ammount
     const savingAmmount = document.getElementById('saving-amount');
-    const savingAmmountText = savingAmmount.innerText;
-    savingAmmount.innerText = saveMoney;
-
+    
+    if(saveInput > 0){
+        savingAmmount.innerText = saveMoney;
+    }else{
+        validation("alert-1");
+        return savingAmmount.innerText = '00';
+    }
     // remaining balance
     const previousBlance = document.getElementById('balance').innerText;
     const remainingCalculate = parseFloat(previousBlance) - saveMoney;
+    
     document.getElementById('remaining-balance').innerText = remainingCalculate;
-
 }
 
 
+
+// =====INPUT VALIDATION FUNCTION PART=========
+function validation(id1){
+    const alert1 = document.getElementById(id1);
+    alert1.style.display = 'block'
+}
+
+// =======REMOVE ALERT FUNCTION======
+function removeAlert(id1){
+    document.getElementById(id1).style.display = 'none'
+}
